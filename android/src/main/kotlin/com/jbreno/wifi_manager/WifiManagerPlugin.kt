8380176b29d3192dc7786d91f25c5f3e1ba63e0b
jbrenorv <joao.breno.ro17@gmail.com>
2022-12-06 11:54:56 -0300

    fix permission bug

--- android/src/main/kotlin/com/jbreno/wifi_manager/WifiManagerPlugin.kt
+++ android/src/main/kotlin/com/jbreno/wifi_manager/WifiManagerPlugin.kt
@@ -1,22 +1,30 @@
 package com.jbreno.wifi_manager
 
+import android.Manifest
+import android.app.Activity
 import android.content.Context
+import android.content.pm.PackageManager
 import androidx.annotation.NonNull
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 
 /** WifiManagerPlugin */
-class WifiManagerPlugin: FlutterPlugin, MethodCallHandler {
+class WifiManagerPlugin: FlutterPlugin, MethodCallHandler, ActivityAware {
   /// The MethodChannel that will the communication between Flutter and native Android
   ///
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
   /// when the Flutter Engine is detached from the Activity
   private lateinit var channel : MethodChannel
   private lateinit var context : Context
+  private lateinit var activity: Activity
   private lateinit var wifiManagerUtil: WifiManagerUtil
 
   override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
@@ -32,11 +40,15 @@ class WifiManagerPlugin: FlutterPlugin, MethodCallHandler {
       "getPlatformVersion" -> result.success("Android ${android.os.Build.VERSION.RELEASE}")
 
       "getConnectionInfo" -> {
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_NETWORK_STATE) != PackageManager.PERMISSION_GRANTED) {
+          ActivityCompat.requestPermissions(activity, arrayOf(Manifest.permission.ACCESS_NETWORK_STATE), 0)
+        }
+
         val ssid = wifiManagerUtil.getConnectionInfo()
         if (ssid != null) {
           result.success("Network SSID: $ssid")
         } else {
-          result.error("UNAVAILABLE", "", null)
+          result.error("UNAVAILABLE", "Unable to get network ssid", null)
         }
       }
 
@@ -46,5 +58,21 @@ class WifiManagerPlugin: FlutterPlugin, MethodCallHandler {
 
   override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
     channel.setMethodCallHandler(null)
+  }
+
+  override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+    activity = binding.activity
+  }
+
+  override fun onDetachedFromActivityForConfigChanges() {
+    TODO("Not yet implemented")
+  }
+
+  override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+    TODO("Not yet implemented")
+  }
+
+  override fun onDetachedFromActivity() {
+    TODO("Not yet implemented")
   }
 }
--- android/src/main/kotlin/com/jbreno/wifi_manager/WifiManagerUtil.kt
+++ android/src/main/kotlin/com/jbreno/wifi_manager/WifiManagerUtil.kt
@@ -1,10 +1,14 @@
 package com.jbreno.wifi_manager
 
+import android.Manifest
 import android.content.Context
+import android.content.pm.PackageManager
 import android.net.ConnectivityManager
 import android.net.wifi.WifiInfo
 import android.net.wifi.WifiManager
 import android.os.Build
+import androidx.core.app.ActivityCompat.requestPermissions
+import androidx.core.content.ContextCompat
 
 class WifiManagerUtil(private val context: Context) {
     private var wifiManager : WifiManager = context.applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
@@ -18,6 +22,7 @@ class WifiManagerUtil(private val context: Context) {
         val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
         val networkCapabilities = connectivityManager.getNetworkCapabilities(null)
         val wifiInfo = networkCapabilities?.transportInfo as WifiInfo?
+
 
         return wifiInfo?.ssid
     }
