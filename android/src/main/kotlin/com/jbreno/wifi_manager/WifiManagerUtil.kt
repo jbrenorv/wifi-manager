2b57f6b6b869ef9315a7d803dbe55fbab810430d
jbrenorv <joao.breno.ro17@gmail.com>
2022-12-07 08:58:54 -0300

    fix bug permissions

--- android/src/main/kotlin/com/jbreno/wifi_manager/WifiManagerUtil.kt
+++ android/src/main/kotlin/com/jbreno/wifi_manager/WifiManagerUtil.kt
@@ -1,18 +1,16 @@
 package com.jbreno.wifi_manager
 
-import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Context
-import android.content.pm.PackageManager
 import android.net.ConnectivityManager
 import android.net.wifi.WifiInfo
 import android.net.wifi.WifiManager
 import android.os.Build
-import androidx.core.app.ActivityCompat.requestPermissions
-import androidx.core.content.ContextCompat
 
 class WifiManagerUtil(private val context: Context) {
     private var wifiManager : WifiManager = context.applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
 
+    @SuppressLint("MissingPermission")
     public fun getConnectionInfo(): String? {
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.S) {
             val wifiInfo = wifiManager.getConnectionInfo()
